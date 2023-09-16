b3791a65c66c9212cca1301ded86b339f86bebe8
suhailkhan123517 <skhan282290@gmail.com>
2023-09-16 08:47:37 +0530

    saturday

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,14 +1,50 @@
+"use client";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setMessage("Invalid Credentials");
+        return;
+      }
+
+      router.replace("dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <form>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
-        <div>message</div>
+        <div>{message}</div>
       </form>
       <Link href="/register">Register</Link>
     </>
--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+"use client";
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 const UserInfo = () => {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <>
       <div>
-        <h1>Name: Suhail</h1>
-        <h1>Email:skhan282290@gmail.com</h1>
-        <button>logout</button>
+        <h1>
+          Name: {session?.user?.name}
+          {session?.user?.lname}
+        </h1>
+        <h1>Email:{session?.user?.email}</h1>
+        <button onClick={() => signOut()}>logout</button>
       </div>
     </>
   );
